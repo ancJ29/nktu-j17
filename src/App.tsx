2581f29eb4aa04ed92cc753e696fa353ef77327f
An <140888823+ancJ29@users.noreply.github.com>
2026-08-07 22:19:33 +0700

    sync@20260807.2219

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { lazy, Suspense, useEffect } from 'react';
 import { RouterProvider } from 'react-router';
